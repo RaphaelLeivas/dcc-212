8ced83e18d5a20f20398a0b841e727a3ae91b6db
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2025-10-26 23:01:43 -0300

    finaliza VPLs adicionais

--- VPL/Adicionais/sol.py
+++ VPL/Adicionais/sol.py
@@ -1,24 +1,22 @@
 import pandas as pd
-import numpy as np
 
-# Carregando o dataframe
-d2 = pd.read_csv('Q2.csv', index_col=0)
+# Sabemos que vários fatores relacionados ao estilo de vida de cada um 
+# podem ter um grande impacto no risco de doenças cardiovasculares ou infarto.
+# Para este exercício, utilizaremos um banco de dados público dos
+# Centros para Controles de Doenças dos Estados Unidos (CDC), que realizou 
+# pesquisa em 2015 a respeito dos hábitos de vida de 253.680 americanos.
+# Este conjunto de dados possui 22 colunas com dados numéricos,
+# em sua maioria categóricos binários (0 para não e 1 para sim).
 
-# b) Utilizando a função 'dependentes' criada por você no exercício anterior (item a da questão 2),
-# verifique se os pares de variáveis (x,y), (y,z) e (x,z) são dependentes no
-# conjunto de dados 'd2'. Se necessário, realize alterações em sua função.
+heart = pd.read_csv('VPL/Adicionais/heart_disease_kaggle.csv')
+print(heart.columns)
+
+# b) Qual a probabilidade de um fumante (smoker = 1) ter sofrido ataque cardíaco 
+# ou possuir doença cardíaca? E qual a probabilidade para um não fumante?
 #
-# Crie três variáveis booleanas para armazenar os resultados:
-# - dep_xy: True se x e y são dependentes, False caso contrário.
-# - dep_yz: True se y e z são dependentes, False caso contrário.
-# - dep_xz: True se x e z são dependentes, False caso contrário.
-#
-# Cole aqui a sua função 'dependentes' da questão 2a.
-# def dependentes(v1, v2, p, df):
-#    ...
+# Armazene os resultados nas variáveis 'prob_fumante' e 'prob_nao_fumante'.
 
+# Escreva seu código aqui
+prob_fumante = None
 
-# Escreva o seu código aqui para calcular os valores das três variáveis
-dep_xy = None
-dep_yz = None
-dep_xz = None
+prob_nao_fumante = None
